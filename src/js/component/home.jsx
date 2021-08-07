0bca43ac7c5b5e0403a68626958835df6ad1ed2f
fabianchs <fabichasola@hotmail.com>
2021-08-07 01:13:38 +0000

    it works

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,21 @@
 import * as htmlToImage from "html-to-image";
 import download from "downloadjs";
 import React from "react";
+import { useState } from "react";
+import { Button, Input, Badge, Table } from "reactstrap";
+import copyImg from "copy-image-clipboard";
+
 const App = () => {
+	const [create_image, setCreateImage] = useState("jelou");
+
 	const onButtonClick = () => {
-		var domElement = document.getElementById("my-node");
+		let domElement = document.getElementById("my-node");
 		htmlToImage
-			.toJpeg(domElement)
+			.toPng(domElement)
 			.then(function(dataUrl) {
 				console.log(dataUrl);
+				copyImg(dataUrl);
+				setCreateImage(<img src={dataUrl}></img>);
 				download(dataUrl, "image.jpeg");
 			})
 			.catch(function(error) {
@@ -15,7 +23,7 @@ const App = () => {
 			});
 	};
 	return (
-		<div className="App bg-light" id="my-node">
+		<div className="App bg-light w-50" id="my-node">
 			<header className="App-header">
 				<img
 					src="https://static.remove.bg/remove-bg-web/3661dd45c31a4ff23941855a7e4cedbbf6973643/assets/start_remove-79a4598a05a77ca999df1dcb434160994b6fde2c3e9101984fb1be0f16d0a74e.png"
@@ -36,9 +44,20 @@ const App = () => {
 					<li>Comida</li>
 					<li>cASA</li>
 				</ul>
+				<Table responsive bordered size="sm" className="text-center">
+					<thead>
+						<tr>
+							<th>elemento</th>
+							<th>elemento</th>
+							<th>elemento</th>
+						</tr>
+					</thead>
+				</Table>
+
 				<br></br>
 				<button onClick={onButtonClick}>Download as JPEG</button>
 			</header>
+			{create_image}
 		</div>
 	);
 };
